f2c6c71e7d67fe9ecd7aa5d4e61e636ae226d87e
JesseMurah <kwekubrown234@gmail.com>
2022-04-19 23:32:31 +0100

    function changes

--- test.dart
+++ test.dart
@@ -1,40 +1,34 @@
 import 'dart:io';
 
-void main() {
-  void check_person_status() {}
+String myFullname() {
+  print("Enter your first name: ");
+  String? firstName = stdin.readLineSync();
 
-  String myFullname() {
-    print("Enter your first name: ");
-    String? firstName = stdin.readLineSync();
+  print("Enter your last name: ");
+  String? lastName = stdin.readLineSync();
 
-    print("Enter your last name: ");
-    String? lastName = stdin.readLineSync();
+  String fullname = firstName! + " " + lastName!;
 
-    String fullname = firstName! + lastName!;
+  print("Enter your age: ");
+  int Age = int.parse(stdin.readLineSync()!);
 
-    String output_string = """
+  print("Have you completed university?: ");
+  String? isCompleted = stdin.readLineSync()!;
+
+  String output_string = """
     First Name = """ +
-        firstName +
-        """Last Name = """ +
-        lastName +
-        """Fullname = """ +
-        fullname;
-    return output_string;
-  }
+      firstName +
+      """\n Last Name = """ +
+      lastName +
+      """\n Fullname = """ +
+      fullname +
+      """\n isCompleted = """ +
+      isCompleted +
+      """\n Age = """ +
+      Age.toString();
+  return output_string;
+}
 
-  int myAge() {
-    print("Enter your age: ");
-    int Age = int.parse(stdin.readLineSync()!);
-    return Age;
-  }
-
-  String comPlete() {
-    print("Have you completed university?: ");
-    String? isCompleted = stdin.readLineSync()!;
-    return isCompleted;
-  }
-
-  print(myAge());
-  print(comPlete());
+void main() {
   print(myFullname());
 }
