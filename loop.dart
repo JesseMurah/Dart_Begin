1b640d67de8c856a8639694ab82fbb0a95dec192
JesseMurah <kwekubrown234@gmail.com>
2022-04-28 04:49:41 +0100

    second update

--- loop.dart
+++ loop.dart
@@ -1,8 +1,31 @@
 void main() {
   //while loops
-  int age = 18;
-  while (age < 50) {
-    print('Age: $age');
-    age++;
-  }
+  //int age = 18;
+  //while (age < 50) {
+  //print('Age: $age');
+  //age++;
+  //}
+  //int num = 1;
+  //for (num; num <= 10; num++) {
+  //print('Number: $num');
+  //}
+  /*List<int> list1 = [10, 20, 30, 40, 50];
+  for (int i in list1) {
+    print('Number: $i');
+  }*/
+
+  /*int a = 10;
+  int max = 100;
+  while (a < max) {
+    print('Number: $a');
+    a++;
+  }*/
+
+  /*int a = 1;
+  int max = 10;
+  do {
+    print('Number: $a');
+    a++;
+  } while (a < max);
+}*/
 }
